20402cf1481997286109cd7a8da4fd4e22038f7b
simpetr <sim.petrosino@gmail.com>
2022-07-01 10:04:25 +0200

    Starting scenes and menu test

---
+++ Assets/Scripts/UI/LoadNextScene.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+namespace UI
+{
+    public class LoadNextScene : MonoBehaviour
+    {
+        public Button changeSceneButton;
+
+        public string nextSceneName = "";
+        // Start is called before the first frame update
+        void Start()
+        {
+            var root = GetComponent<UIDocument>().rootVisualElement;
+            changeSceneButton = root.Q<Button>("StartButton");
+            changeSceneButton.clicked += LoadScene;
+        }
+
+        private void LoadScene()
+        {
+            if(nextSceneName != "" )
+               SceneManager.LoadScene(nextSceneName);
+        }
+    }
+}
