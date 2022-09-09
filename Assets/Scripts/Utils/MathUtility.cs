00889bca19b37c51b3532e1563c76782f8ae21d6
simpetr <wuah16@gmail.com>
2022-09-09 09:18:31 +0200

    $139: Add Math utility with basic ease functions

---
+++ Assets/Scripts/Utils/MathUtility.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace Utils
+{
+    public static class MathUtility
+    {
+        // https://easings.net/it#
+        public enum Ease
+        {
+            Linear,
+            EaseInSin,
+            EaseOutSin,
+            EaseInQuart,
+            EaseInQuint
+        }
+
+        public delegate float EaseFunction(float value);
+        
+        public static float Linear(float value) => value;
+        
+        public static float EaseInSin(float value) => 1 - Mathf.Cos((value * Mathf.PI) / 2);
+
+        public static float EaseOutSin(float value) => Mathf.Sin((value * Mathf.PI) / 2);
+        
+        public static float EaseInQuart(float value) => value * value * value * value;
+        
+        public static float EaseInQuint(float value) => value * value * value * value * value;
+
+        public static EaseFunction GetEaseFunctionByName(Ease name)
+        {
+            switch (name)
+            {
+                case Ease.Linear: return Linear;
+                case Ease.EaseInSin: return EaseInSin;
+                case Ease.EaseOutSin: return EaseOutSin;
+                case Ease.EaseInQuart: return EaseInQuart;
+                case Ease.EaseInQuint: return EaseInQuint;
+                default: return Linear;
+            }
+        }
+        
+    }
+}
